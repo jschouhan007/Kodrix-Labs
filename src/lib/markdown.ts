7e96d21b5e4a339970e371f1231b4a0cb8d4fc84
Jaideep Singh Chouhan <jscofficially@gmail.com>
2026-05-22 18:37:15 +0530

    Delete src directory

--- src/lib/markdown.ts
+++
@@ -1,7 +0,0 @@
-import { marked } from "marked";
-
-marked.setOptions({ gfm: true, breaks: false });
-
-export function renderMarkdown(md: string): string {
-  return marked.parse(md ?? "", { async: false }) as string;
-}
--- src/lib/site.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_URL = "https://privacy-guide-nest.lovable.app";
-export const SITE_NAME = "Kodrix Labs";
