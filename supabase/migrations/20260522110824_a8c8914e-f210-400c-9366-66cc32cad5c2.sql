df9b9d6e57c10894205b2ae1f69d10054aea817c
Jaideep Singh Chouhan <jscofficially@gmail.com>
2026-05-22 18:37:28 +0530

    Delete supabase directory

--- supabase/migrations/20260522110824_a8c8914e-f210-400c-9366-66cc32cad5c2.sql
+++
@@ -1,5 +0,0 @@
-
-ALTER FUNCTION public.set_updated_at() SET search_path = public;
-
-REVOKE EXECUTE ON FUNCTION public.has_role(uuid, app_role) FROM PUBLIC, anon, authenticated;
-REVOKE EXECUTE ON FUNCTION public.handle_new_user_role() FROM PUBLIC, anon, authenticated;
